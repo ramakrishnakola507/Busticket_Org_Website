331b749af9372abe32180bd10cfe8f6012d4e4a2
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2025-04-07 17:34:55 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ import ProtectedRoute from './utils/ProtectedRoute';
 import SignIn from './Pages/authPages/SignIn';
 import ContentMain from './components/ContentMain/ContentMain';
 import '../src/layout/Content/Content.css';
-import CreateOrganization from './Pages/organization/CreateOrganization';
-import CreateSuperadmin from './Pages/organization/CreateSuperadmin';
 
-
-import ViewOrganizations from './Pages/organization/ViewOrganization';
-import Viewgodusers from './Pages/organization/Viewgodusers';
-import Viewusers from './Pages/organization/Viewusers';
-import Viewlogs from './Pages/organization/Viewlogs';
-import UpdatePassword from './Pages/settings/UpdatedPassword';
-import CreateGod from './Pages/organization/CreateGod';
 
 
 
@@ -34,15 +25,6 @@ function App() {
         <Route element={<BaseLayout />}>
           <Route path="/" element={<ContentMain />} />
          
-          <Route path="/create-organization" element={<CreateOrganization/>} />
-          <Route path="/create-superadmin" element={<CreateSuperadmin/>} />
-         
-          <Route path="/view-orgnization" element={<ViewOrganizations/>} /> 
-          <Route path="/view-godusers" element={<Viewgodusers/>} /> 
-          <Route path="/view-users" element={<Viewusers/>} /> 
-          <Route path="/view-logs" element={<Viewlogs/>} /> 
-          <Route path="/update-password" element={<UpdatePassword/>} /> 
-          <Route path="/create-god" element={<CreateGod/>} /> 
           
           {/* Add other protected routes here */}
 
